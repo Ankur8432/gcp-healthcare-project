133ede96c406d1a72c38e3811cb21a722c5bdff8
Ankur Karpe <ankurskarpe@gmail.com>
2025-03-31 09:19:27 +0530

    cccc

--- data/INGESTION/claims.py
+++ data/INGESTION/claims.py
@@ -7,9 +7,9 @@ spark = SparkSession.builder \
                     .getOrCreate()
 
 # configure variables
-BUCKET_NAME = "healthcare-bucket-22032025"
+BUCKET_NAME = "health_care_buckets"
 CLAIMS_BUCKET_PATH = f"gs://{BUCKET_NAME}/landing/claims/*.csv"
-BQ_TABLE = "avd-databricks-demo.bronze_dataset.claims"
+BQ_TABLE = "avd-group-gcp.bronze_dataset.claims"
 TEMP_GCS_BUCKET = f"{BUCKET_NAME}/temp/"
 
 # read from claims source
